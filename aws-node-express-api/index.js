c1e4c501e66ae94f935d1a5c38e405f8e42ad15b
shalom talker <shalom604@gmail.com>
2021-06-23 11:13:42 +0300

    fixing 'cannot GET null' issue with noPrependStageInUrl option to sls-offline

--- aws-node-express-api/index.js
+++ aws-node-express-api/index.js
@@ -2,13 +2,19 @@ const serverless = require("serverless-http");
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res, next) => {
+app.get("/", async (req, res, next) => {
   return res.status(200).json({
     message: "Hello from root!",
   });
 });
 
-app.get("/hello", (req, res, next) => {
+app.get("/test", async (req, res, next) => {
+  return res.status(200).json({
+    message: "Hello from test!",
+  });
+});
+
+app.get("/hello", async (req, res, next) => {
   return res.status(200).json({
     message: "Hello from path!",
   });
